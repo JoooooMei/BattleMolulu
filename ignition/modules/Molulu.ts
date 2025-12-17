b49f73f3fb978c7b2b1b088e7f82054acc72b066
JoooooMei <c.johan.meisel@gmail.com>
2025-12-17 14:07:10 +0100

    Ignore Hardhat Ignition artifacts

--- ignition/modules/Molulu.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-export default buildModule('MoluluModule', (m) => {
-  const molulu = m.contract('Molulu');
-
-  return { molulu };
-});
--- ignition/modules/MoluluV2.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-export default buildModule('MoluluV2Module', (m) => {
-  const moluluv2 = m.contract('MoluluV2');
-
-  return { moluluv2 };
-});
