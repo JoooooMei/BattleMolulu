cb5860a51aec863bfff5a82560b40d52c809940b
JoooooMei <c.johan.meisel@gmail.com>
2025-12-17 13:59:56 +0100

    update gameSimulator

--- backend/src/models/GameSimulator.mjs
+++ backend/src/models/GameSimulator.mjs
@@ -1,5 +1,13 @@
 import { parseEther } from 'ethers';
 import MoluluRepository from '../repository/MoluluRepository.mjs';
+
+import {
+  getEligibleMolulus,
+  getParticipatingMolulus,
+  getTournamentStartTime,
+} from '../services/TournamentService.mjs';
+import { destructParticipants } from '../utils/helpers.mjs';
+
 export default class GameSimilator {
   constructor({ players }) {
     this.players = players;
@@ -8,16 +16,14 @@ export default class GameSimilator {
 
     //prices
 
-    this.payHat = parseEther('0.02');
+    this.payHat = parseEther('0.01');
+    this.buyGlasses = parseEther('0.2');
     this.payCape = parseEther('0.03');
     this.payBoots = parseEther('0.025');
     this.payRing = parseEther('0.05');
-
-    this.wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   async createWallets() {
-    await this.wait(1000);
     this.players.forEach((player) => {
       const wallet = new MoluluRepository(player);
       this.wallets.push(wallet);
@@ -27,7 +33,6 @@ export default class GameSimilator {
   }
 
   async mintMolusuls() {
-    await this.wait(1000);
     for (let i = 0; i < this.wallets.length; i++) {
       const molulu = await this.wallets[i].mintMolulu();
 
@@ -36,17 +41,42 @@ export default class GameSimilator {
   }
 
   async buyAccessory() {
-    await this.wait(1000);
-    console.log('Biying accessory');
-    const hat = await this.wallets[0].buyAccessory(1, 'Hat', this.payHat);
+    console.log('Buying accessory');
+    // const userOne = await this.wallets[0].buyAccessory(1, 'Hat', this.payHat);
+    const userTwo = await this.wallets[1].buyAccessory(2, 'Ring', this.payRing);
 
-    console.log('bought hat', hat);
+    console.log('bought Ring', userTwo);
   }
 
   async getLiquidityContributors() {
-    await this.wait(1000);
     console.log('in  contributions');
     const contributions = await this.wallets[0].fetchLiquidityContributors();
     console.log('comntributions: ', contributions);
+  }
+
+  async allAccessoryPurchases() {
+    console.log('in accessory purchases');
+    const purchases = await this.wallets[0].fetchAllAccessoryPurchases();
+
+    console.log('purchases', purchases);
+  }
+
+  async participants() {
+    const repo = this.wallets[0];
+    const startTime = await getTournamentStartTime(repo);
+    const participating = await getEligibleMolulus(repo, startTime);
+
+    //console.log('Participants:', JSON.stringify(participants, null, 2));
+    console.dir(participating, { depth: null });
+    console.dir(participants, { depth: null });
+  }
+
+  async participatingMolulus() {
+    const repo = this.wallets[0];
+    const startTime = await getTournamentStartTime(repo);
+    const participating = await getParticipatingMolulus(repo, startTime);
+
+    console.log('Participating Molulus: ', participating);
+    console.dir(participating, { depth: null });
   }
 }
